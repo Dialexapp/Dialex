c8db4bb1283ec4f73d45c7d2d35dab3104cc2446
Dialexapp <dialexdev@gmail.com>
2026-06-28 01:51:54 +0200

    Refactor dizionario structure and add new entries

--- dizionario.js
+++ dizionario.js
@@ -1,32 +1,26 @@
-window.dizionario = {
-
-    // base umana
-    "ciao": "uè",
-    "amico": "fratè",
-    "amica": "sore",
-    "voglio": "voglio",
-    "devo": "aggia",
-    "devi": "aje",
-    "andare": "ià",
-    "casa": "casa",
-
-    // verbi più naturali
-    "mangiare": "magnà",
-    "bere": "bevè",
-    "dire": "dicere",
-    "fare": "fà",
-    "vedere": "vedé",
-
-    // frasi comuni
-    "non lo so": "nun o sacce",
-    "che fai": "che staje facenno",
-    "sto bene": "stò bbene",
-    "come stai": "comme staje",
-    "tutto ok": "tutto a posto",
-
-    // vita quotidiana
-    "soldi": "sorde",
-    "lavoro": "travaglio",
-    "ragazzo": "guaglione",
-    "ragazza": "guagliona"
-};
+let dizionario = {
+ "ciao": "uè",
+  "come stai": "come staje",
+  "sto bene": "stong' bbuon",
+  "tutto bene": "tutt'apposto",
+  "che fai": "che staie facenno",
+  "dove vai": "addò vaie",
+  "non lo so": "nun 'o ssaccio",
+  "grazie": "grazie",
+  "prego": "prego",
+  "scusa": "scusame",
+  "buongiorno": "buongiorno",
+  "buonasera": "buonasera",
+  "quanto costa": "quanto costa",
+  "ho fame": "tengo fame",
+  "ho sete": "tengo sete",
+  "sono stanco": "sto stanco",
+  "vieni qui": "vieni ccà",
+  "andiamo": "jamm",
+  "non capisco": "nun capisco",
+  "è bello": "è bbello",
+  "è brutto": "è bbrutto",
+  "che succede": "che succede",
+  "tutto ok": "tutto bbuon",
+  "va bene": "va bbuon"
+ };
