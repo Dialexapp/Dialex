d1250285753ece28385634087a0b495146ba9999
Dialexapp <dialexdev@gmail.com>
2026-06-28 03:33:51 +0200

    Refactor traduci function and add new functions

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function traduci() {
-     alert("OK CLICK FUNZIONA");
+  alert("TRADUCI OK");
+}
+
+function scambiaLingue() {
+  alert("SCAMBIA OK");
+}
+
+function reset() {
+  alert("RESET OK");
+}
+
+function addRipple() {}
+
+function toggleDark() {
+  document.body.classList.toggle("dark");
 }
