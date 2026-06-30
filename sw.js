fcd7e3f8b40b496337f1e0d4797dac940451e58c
Dialexapp <dialexdev@gmail.com>
2026-07-01 01:15:21 +0200

    Add caching functionality to service worker
    Implement service worker caching for files during install and fetch events.

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
-self.addEventListener("fetch", () => {});
+const CACHE = "dialex-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./script.js",
+        "./dizionario.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
+  );
+});
